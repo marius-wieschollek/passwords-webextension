4039be9149497633cb6b525d21d6176453043f77
Marius David Wieschollek <passwords.public@mdns.eu>
2020-01-06 23:46:39 +0100

    Added extension settings ui

---
+++ src/vue/App/Options.vue
@@ -0,0 +1,79 @@
+<template>
+    <div id="options">
+        <tabs :tabs="tabs">
+            <accounts slot="accounts"/>
+            <div slot="theme">
+                theme
+            </div>
+            <div slot="other">
+                other
+            </div>
+        </tabs>
+    </div>
+</template>
+
+<script>
+    import Tabs from '@vue/Components/Tabs';
+    import Accounts from '@vue/Components/Options/Accounts';
+
+    export default {
+        el        : '#app',
+        components: {Tabs, Accounts},
+
+        computed: {
+            tabs() {
+                return {
+                    accounts: {
+                        label: 'SettingsTabAccounts'
+                    },
+                    theme   : {
+                        label: 'SettingsTabTheming'
+                    },
+                    other   : {
+                        label: 'SettingsTabOther'
+                    }
+                };
+            }
+        }
+    };
+</script>
+
+<style lang="scss">
+    body {
+        select,
+        input[type=text] {
+            background-color : var(--main-background-color);
+            color            : var(--element-secondary-text-color);
+            border           : 1px solid var(--element-secondary-border-color);
+            border-radius    : 3px;
+            font-weight      : normal;
+            cursor           : text;
+            padding          : 3px;
+
+            &:hover {
+                background-color : var(--main-background-color);
+                color            : var(--element-secondary-hover-text-color);
+                border           : 1px solid var(--element-secondary-hover-border-color);
+            }
+        }
+
+        select {
+            cursor : pointer;
+        }
+
+        input[type=button] {
+            background-color : var(--element-secondary-background-color);
+            color            : var(--element-secondary-text-color);
+            border           : 1px solid var(--element-secondary-border-color);
+            border-radius    : 3px;
+            font-weight      : normal;
+            cursor           : pointer;
+
+            &:hover {
+                background-color : var(--element-secondary-hover-background-color);
+                color            : var(--element-secondary-hover-text-color);
+                border           : 1px solid var(--element-secondary-hover-border-color);
+            }
+        }
+    }
+</style>
---
+++ src/vue/Components/Options/Accounts.vue
@@ -0,0 +1,168 @@
+<template>
+    <div>
+        <div class="account-options">
+            <translate tag="label" for="master-account" say="SettingsAccountsMain"/>
+            <select id="master-account">
+                <option v-for="server in servers" :key="server.getId()" :value="server.getId()">
+                    {{server.getLabel()}}
+                </option>
+            </select>
+        </div>
+        <foldout :tabs="serverNames">
+            <form v-for="server in servers"
+                  :key="server.getId()"
+                  :slot="server.getId()"
+                  class="account-settings"
+                  v-on:submit.prevent="saveServer(server)">
+                <translate tag="label" :for="`${server.getId()}-name`" say="AccountLabel"/>
+                <input type="text" :id="`${server.getId()}-name`" v-model="server.getLabel()"/>
+                <translate tag="label" :for="`${server.getId()}-url`" say="AccountBaseUrl"/>
+                <input type="text" :id="`${server.getId()}-url`" v-model="server.getBaseUrl()"/>
+                <translate tag="label" :for="`${server.getId()}-user`" say="AccountUser"/>
+                <input type="text" :id="`${server.getId()}-user`" v-model="server.getUser()"/>
+                <translate tag="label" :for="`${server.getId()}-token`" say="AccountToken"/>
+                <input type="button" value="Change" :id="`${server.getId()}-token`"/>
+            </form>
+            <icon v-for="server in servers"
+                  :key="server.getId()"
+                  :slot="`${server.getId()}-tab-open`"
+                  icon="save"
+                  v-on:click.prevent="saveServer(server)"/>
+            <icon v-for="server in servers"
+                  :key="server.getId()"
+                  :slot="`${server.getId()}-tab-closed`"
+                  icon="trash-alt"
+                  v-on:click.prevent="deleteServer(server)"/>
+
+            <form v-if="showNewForm"
+                  slot="addserver"
+                  class="account-settings"
+                  v-on:submit.prevent="createServer($event)">
+                <translate tag="label" for="new-name" say="AccountLabel"/>
+                <input type="text" id="new-name" v-model="newServer.label" required/>
+                <translate tag="label" for="new-url" say="AccountBaseUrl"/>
+                <input type="text" id="new-url" v-model="newServer.baseUrl" required/>
+                <translate tag="label" for="new-user" say="AccountUser"/>
+                <input type="text" id="new-user" v-model="newServer.user" required/>
+                <translate tag="label" for="new-token" say="AccountToken"/>
+                <input type="text"
+                       value="Change"
+                       id="new-token"
+                       v-model="newServer.token"
+                       required
+                       pattern="([A-Za-z0-9]{5}-?){5}"
+                       placeholder="xxxxx-xxxxx-xxxxx-xxxxx-xxxxx"/>
+                <input type="submit"/>
+            </form>
+        </foldout>
+        <translate tag="button" say="Add a server" @click="addServer" v-if="!showNewForm"/>
+    </div>
+</template>
+
+<script>
+    import Translate from '@vue/Components/Translate';
+    import Foldout from '@vue/Components/Foldout';
+    import Icon from '@vue/Components/Icon';
+    import MessageService from '@js/Services/MessageService';
+
+    export default {
+        components: {Foldout, Translate, Icon},
+        data() {
+            return {
+                servers    : [],
+                showNewForm: false,
+                newServer  : {
+                    label  : '',
+                    baseUrl: '',
+                    user   : '',
+                    token  : ''
+                }
+            };
+        },
+
+        created() {
+            this.loadData();
+        },
+
+        computed: {
+            serverNames() {
+                let names = {};
+
+                for(let server of this.servers) {
+                    names[server.getId()] = server.getLabel();
+                }
+
+                if(this.showNewForm) {
+                    names.addserver = 'New Account';
+                }
+
+                return names;
+            }
+        },
+
+        methods: {
+            async loadData() {
+                try {
+                    let message = await MessageService.send({type: 'server.list'});
+                    this.servers = message.getPayload();
+                } catch(e) {
+
+                }
+            },
+            addServer() {
+                this.showNewForm = true;
+            },
+
+            /**
+             *
+             * @param {Server} server
+             */
+            async saveServer(server) {
+                alert('This is not yet implemented. Please use delete and create.');
+            },
+
+            /**
+             * @param {Server} server
+             */
+            async deleteServer(server) {
+                if(confirm(`Do you really want to delete ${server.getLabel()}?`)) {
+                    let message = await MessageService.send({type: 'server.delete', payload: {server: server.getId()}});
+                    if(message.getType() === 'delete.success') {
+                        this.loadData();
+                    } else {
+                        alert(message.getPayload().message);
+                        this.loadData();
+                    }
+                }
+            },
+
+            /**
+             *
+             * @param {Event} event
+             */
+            async createServer(event) {
+                let message = await MessageService.send({type: 'server.create', payload: this.newServer});
+                if(message.getType() === 'server.item') {
+                    this.servers.push(message.getPayload());
+                    this.showNewForm = false;
+                } else {
+                    alert(message.getPayload().message);
+                }
+            }
+        }
+    };
+</script>
+
+<style lang="scss">
+    .account-options,
+    .account-settings {
+        display               : grid;
+        grid-template-columns : 2fr 1fr;
+        grid-row-gap          : 0.5rem;
+        padding               : 0.5rem 1rem;
+
+        label {
+            line-height : 2rem;
+        }
+    }
+</style>
