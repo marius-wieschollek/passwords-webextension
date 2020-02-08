dd4cdbbdbc25dd3721b0e3187492f454decae9ed
Marius David Wieschollek <passwords.public@mdns.eu>
2020-02-08 13:15:02 +0100

    Add collected passwords review and save

--- src/vue/Components/Popup/Collected.vue
+++ src/vue/Components/Popup/Collected.vue
@@ -1,21 +1,47 @@
 <template>
     <div class="collected-container">
-        <password v-for="item of items" :key="item.getId()" :password="item.getPassword()"></password>
+        <foldout :tabs="tabs" :translate="false">
+            <icon icon="save" :slot="`${item.getId()}-tab`" :key="item.getId()" v-for="item of items" @click="save(item)"/>
+            <div :slot="item.getId()" :key="item.getId()" v-for="item of items">
+                <div v-for="(field, name) in item.getFields()">
+                    {{name}}: {{field}}
+                </div>
+            </div>
+        </foldout>
         <translate tag="div" class="no-results" say="NoCollectedPasswords" v-if="items.length === 0"/>
     </div>
 </template>
 
 <script>
+    import Icon from '@vue/Components/Icon';
+    import Foldout from '@vue/Components/Foldout';
     import Translate from '@vue/Components/Translate';
     import MiningClient from '@js/Queue/Client/MiningClient';
-    import Password from '@vue/Components/List/Item/Password';
 
     export default {
-        components: {Password, Translate},
+        components: {Translate, Foldout, Icon},
         data() {
             return {
                 items: MiningClient.getItems()
             };
+        },
+
+        computed  : {
+            tabs() {
+                let tabs = {};
+                for(let item of this.items) {
+                    tabs[item.getId()] = item.getLabel();
+                }
+
+                return tabs;
+            }
+        },
+
+        methods: {
+            async save(item) {
+                await MiningClient.solveItem(item);
+                this.items = MiningClient.getItems();
+            }
         }
     };
 </script>
