2aa432ebe846ac6623d7ac2782036627cc08feaa
Marius David Wieschollek <passwords.public@mdns.eu>
2020-02-17 14:43:45 +0100

    Simplify search controller

--- src/js/Controller/Password/Search.js
+++ src/js/Controller/Password/Search.js
@@ -1,7 +1,5 @@
-import QueryParser from 'search-query-parser';
-import SearchQuery from '@js/Search/Query/SearchQuery';
 import AbstractController from '@js/Controller/AbstractController';
-import TabManager from '@js/Manager/TabManager';
+import TextQuery from '@js/Search/Query/TextQuery';
 
 export default class Search extends AbstractController {
 
@@ -11,66 +9,10 @@ export default class Search extends AbstractController {
      * @param {Message} reply
      */
     async execute(message, reply) {
-        let input = message.getPayload().query;
-        let query = QueryParser.parse(input, {keywords: ['id', 'tag', 'folder', 'host', 'url', 'server'], tokenize: true});
-        let search = new SearchQuery()
-            .type('password')
-            .score(0.2)
-            .limit(15);
-
-        for(let key in query) {
-            if(!query.hasOwnProperty(key) || key === 'offsets' || key === 'exclude') continue;
-
-            if(key === 'text') {
-                this._addTextCondition(search, query);
-            } else {
-                this._addFieldCondition(query, key, search);
-            }
-        }
+        let input = message.getPayload().query,
+            query = new TextQuery(input);
 
         reply.setType('password.items')
-            .setPayload(search.execute());
-
-        TabManager.set('search.query', input);
-        TabManager.set('search.results', search)
-    }
-
-    /**
-     *
-     * @param {Object} query
-     * @param {String} key
-     * @param {SearchQuery} search
-     * @private
-     */
-    _addFieldCondition(query, key, search) {
-        let value = query[key];
-
-        if(Array.isArray(value)) {
-            search.where(
-                search.field(key).in(value)
-            );
-        } else {
-            search.where(
-                search.field(key).equals(value)
-            );
-        }
-    }
-
-    /**
-     *
-     * @param {Object} query
-     * @param {SearchQuery} search
-     * @private
-     */
-    _addTextCondition(search, query) {
-        let condition = search.and();
-
-        for(let value of query.text) {
-            condition.where(
-                search.field('text').contains(value)
-            );
-        }
-
-        search.where(condition);
+            .setPayload(query.execute());
     }
 }
