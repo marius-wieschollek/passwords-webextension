5dc683c6e6b2b3d4a830ae42183a229205f4a8e8
Marius David Wieschollek <passwords.public@mdns.eu>
2020-02-26 13:30:19 +0100

    Fix reloading server
    Signed-off-by: Marius David Wieschollek <passwords.public@mdns.eu>

--- src/js/Controller/Server/Reload.js
+++ src/js/Controller/Server/Reload.js
@@ -1,12 +1,12 @@
 import AbstractController from '@js/Controller/AbstractController';
-import SearchManager from '@js/Manager/SearchManager';
 import ServerRepository from '@js/Repositories/ServerRepository';
+import ServerManager from '@js/Manager/ServerManager';
 
 export default class Reload extends AbstractController {
 
     async execute(message, reply) {
         let server = await ServerRepository.findById(message.getPayload());
-        await SearchManager.reloadServer(server);
+        await ServerManager.reloadServer(server);
         reply.setPayload(true);
     }
 }
