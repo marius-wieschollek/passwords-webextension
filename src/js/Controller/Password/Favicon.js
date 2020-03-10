170f92be70d4f7d22a1224def2844b5c0249404c
Marius David Wieschollek <passwords.public@mdns.eu>
2020-03-10 14:19:03 +0100

    Use badge icon as favicon fallback
    Signed-off-by: Marius David Wieschollek <passwords.public@mdns.eu>

--- src/js/Controller/Password/Favicon.js
+++ src/js/Controller/Password/Favicon.js
@@ -1,6 +1,7 @@
 import AbstractController from '@js/Controller/AbstractController';
 import SearchIndex from '@js/Search/Index/SearchIndex';
 import SystemService from '@js/Services/SystemService';
+import ThemeService from '@js/Services/ThemeService';
 
 export default class Favicon extends AbstractController {
 
@@ -11,10 +12,13 @@ export default class Favicon extends AbstractController {
         let model = SearchIndex.getItem(password);
 
         if(model !== null) {
-            reply.setPayload(model.getFaviconUrl(size))
+            reply.setPayload(model.getFaviconUrl(size));
         } else {
-            let icon = await SystemService.getBrowserApi().runtime.getURL('img/favicon-fallback.svg');
-            reply.setPayload(icon)
+            let icon = await ThemeService.getBadgeIcon();
+            if(icon === null) {
+                icon = await SystemService.getBrowserApi().runtime.getURL('img/passwords-new-dark.svg');
+            }
+            reply.setPayload(icon);
         }
     }
 }
