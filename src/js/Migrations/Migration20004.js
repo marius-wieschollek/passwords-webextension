cdc0ebb9c286302b07ed8455ca14424b83b2b94d
Marius David Wieschollek <passwords.public@mdns.eu>
2023-10-22 13:23:40 +0200

    Refactor Migration 20004
    Signed-off-by: Marius David Wieschollek <passwords.public@mdns.eu>

--- src/js/Migrations/Migration20004.js
+++ src/js/Migrations/Migration20004.js
@@ -6,7 +6,35 @@ export default class Migration20004 {
     async run() {
         StorageService.stop();
         await this._removeOldVariables();
-        let instanceId  = self.crypto.randomUUID(),
+        let instanceId = self.crypto.randomUUID();
+        await this._migrateSyncData(instanceId);
+        await this._migrateLocalData(instanceId);
+    }
+
+    async _migrateLocalData(instanceId) {
+        let api = SystemService.getBrowserApi().storage;
+        let oldLocalData = await api.local.get(),
+            newLocalData = {};
+
+        for(let key in oldLocalData) {
+            if(!oldLocalData.hasOwnProperty(key) || key === 'version') continue;
+
+            try {
+                newLocalData[key] = {time: Date.now(), instance: instanceId, value: JSON.parse(oldLocalData[key]), version: 1};
+            } catch(e) {
+                ErrorManager.error(`Failed to migrate ${key} from local storage`);
+            }
+        }
+
+        await api.local.clear();
+        newLocalData.keys = Object.keys(newLocalData);
+        newLocalData.instance = instanceId;
+        await api.local.set(newLocalData);
+        await StorageService.init();
+    }
+
+    async _migrateSyncData(instanceId) {
+        let
             api         = SystemService.getBrowserApi().storage,
             oldSyncData = await api.sync.get(),
             newSyncData = {};
@@ -23,24 +51,6 @@ export default class Migration20004 {
         await api.sync.clear();
         newSyncData.keys = Object.keys(newSyncData);
         await api.sync.set(newSyncData);
-
-        let oldLocalData = await api.local.get(),
-            newLocalData = {instance: instanceId};
-
-        for(let key in oldLocalData) {
-            if(!oldLocalData.hasOwnProperty(key) || key === 'version') continue;
-
-            try {
-                newLocalData[key] = {time: Date.now(), instance: instanceId, value: JSON.parse(oldLocalData[key]), version: 1};
-            } catch(e) {
-                ErrorManager.error(`Failed to migrate ${key} from local storage`);
-            }
-        }
-
-        await api.local.clear();
-        newLocalData.keys = Object.keys(newLocalData);
-        await api.local.set(newLocalData);
-        await StorageService.init();
     }
 
     async _removeOldVariables() {
