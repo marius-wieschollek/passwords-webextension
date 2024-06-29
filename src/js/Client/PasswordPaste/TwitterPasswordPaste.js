0b30e23e563781a04724898bfdf123c2b634c97e
Marius David Wieschollek <passwords.public@mdns.eu>
2024-06-29 16:47:23 +0200

    Fix twitter login
    Signed-off-by: Marius David Wieschollek <passwords.public@mdns.eu>

--- src/js/Client/PasswordPaste/TwitterPasswordPaste.js
+++ src/js/Client/PasswordPaste/TwitterPasswordPaste.js
@@ -3,35 +3,38 @@ import AbstractPasswordPaste from "@js/Client/PasswordPaste/AbstractPasswordPast
 export default class TwitterPasswordPaste extends AbstractPasswordPaste {
 
     canHandle() {
-        return location.href.startsWith('https://twitter.com/') &&
-            (
-                (
-                    !this._passwordPasteRequest.isAutofill() &&
-                    document.querySelector('a[href="/login"]') !== null
-                ) ||
-                document.querySelector('input[autocomplete="username"]') !== null
-            );
+        return (
+                   location.href.startsWith('https://twitter.com/') ||
+                   location.href.startsWith('https://x.com/')
+               ) &&
+               (
+                   (
+                       !this._passwordPasteRequest.isAutofill() &&
+                       document.querySelector('a[href="/login"]') !== null
+                   ) ||
+                   document.querySelector('input[autocomplete="username"]') !== null
+               );
     }
 
     async handle() {
         if(document.querySelector('input[name="password"]') === null) {
             let userInput;
             try {
-                let time = location.href === 'https://twitter.com/i/flow/login' ? 10000:1000;
+                let time = (location.href === 'https://twitter.com/i/flow/login' || location.href === 'https://x.com/i/flow/login') ? 10000:1000;
                 userInput = await this._waitForElement('input[autocomplete="username"]', time);
-            } catch (e) {
-                if (this._passwordPasteRequest.isAutofill()) {
+            } catch(e) {
+                if(this._passwordPasteRequest.isAutofill()) {
                     return false;
                 }
 
                 let loginButton = document.querySelector('a[href="/login"]');
-                if (loginButton === null) {
+                if(loginButton === null) {
                     return false;
                 }
                 this._simulateClick(loginButton);
                 try {
                     userInput = await this._waitForElement('input[autocomplete="username"]', 10000);
-                } catch (e) {
+                } catch(e) {
                     return false;
                 }
             }
@@ -43,7 +46,7 @@ export default class TwitterPasswordPaste extends AbstractPasswordPaste {
         let passwordInput = await this._waitForElement('input[name="password"]', 10000);
         this._insertTextIntoField(passwordInput, this._passwordPasteRequest.getPassword());
 
-        if (!this._passwordPasteRequest.isAutofill() && this._passwordPasteRequest.isSubmit()) {
+        if(!this._passwordPasteRequest.isAutofill() && this._passwordPasteRequest.isSubmit()) {
             this._simulateEnter(passwordInput);
         }
 
